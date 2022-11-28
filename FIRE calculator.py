ec2bb80803d55c2b96fa5392a811fbef9822187b
LaplacesAngel <118093209+LaplacesAngel@users.noreply.github.com>
2022-11-28 15:46:06 -0600

    Add files via upload

--- FIRE calculator.py
+++ FIRE calculator.py
@@ -22,7 +22,9 @@
 
 #print ("That means you will need to invest " + y " per month to retire by 50")
 print ("How much do you need to invest per month to retire when you want?")
-print ("per this website? https://walletburst.com/tools/coast-fire-calc/")
+print ("Go to this website which will help you answer the questions")
+print ("https://walletburst.com/tools/coast-fire-calc/")
+print ("How much did you need to end up contributing per month to reach FIRE?")
 retAnswerperMonth = input ()
 num = retAnswerperMonth
 converted_num = int(num)
@@ -31,22 +33,36 @@ retAnswerinvestPeryear = converted_num * 12
 print ( )
 print ("That means you'll need to invest " + str(f"${retAnswerinvestPeryear:,.2f}") + " per year")
 print ( )
+print ("Now let's figure out what percentage of your income you'll need to invest.")
+print("What percentage of your post tax income do you want to contribute to your retirement?")
+print ("(e.g. if you only want to contribute 20% of your post tax income enter the number 20)")
+incomePercent = input()
+num = incomePercent
+converted_num_2 = int(num)
+
 x= retAnswerinvestPeryear
-post_tax_income = x / .3
+post_tax_income = x / (converted_num_2/100)
 post_tax_income_in_dollars = (f"${post_tax_income:,.2f}")
-print ("The amount of post tax income you will need to make while investing only 30 percent of  your income is " + post_tax_income_in_dollars)
+print ( )
+print ("The amount of post-tax income you will need to make per year while investing only " + incomePercent + " percent of  your income is " + post_tax_income_in_dollars)
 living_money = post_tax_income - x
 living_money_in_dollars = f"${living_money:,.2f}"
-print (' ')
-print ( "That will leave you with " + living_money_in_dollars + " to live on per year")
-print ( )
-pretax_income = (int(post_tax_income) / .65)
+print ( "That will leave you with " + living_money_in_dollars + " to live on per year after investments and taxes.")
+print ("What do you expect your overall tax rate to be?")
+print ("If you think it'll be 35% then enter 35")
+print ("(this is a good estimate number if you don't know)")
+taxRate = input()
+num = taxRate
+converted_num_3 = int(num)
+pretax_income = (int(post_tax_income) / (1 - (converted_num_3/100)))
 pretax_income_in_dollars = f"${pretax_income:,.2f}"
+print ("This means you'll need a pretax income of " + pretax_income_in_dollars + " per year if you only want to invest " + incomePercent  + " percent of your income per year")
 print ("How much do you want to live off per year during retirement?")
 print ("This will be the same number as you put in the website.")
 retirementLive = input()
 num = retirementLive
-converted_num_2 = int(num)
-print ("Assuming a 35% overall tax rate")
-print ("The amount of pretax income you will need in order to live off of is " + str(f"${converted_num_2:,.2f}") + " a year is " + pretax_income_in_dollars)
+converted_num_4 = int(num)
+print ("So to sum things up")
+print ("The amount of pretax income you'll need in order to have " + str(f"${converted_num_4:,.2f}") + " per year paid to you in retirement is ")
+print( pretax_income_in_dollars + " assuming a " + taxRate + " tax rate")
 
