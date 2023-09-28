a59f11aa43bd3264371f45a7215e1121c400120e
paul <paulentz@gmail.com>
2023-09-28 00:31:46 -0500

    more basic string functions

--- more_practice_file.py
+++ more_practice_file.py
@@ -1,4 +1,10 @@
-name = 'paul'
-name = name.capitalize()
+first_name = 'paul'
+last_name = 'lentz'
+note = 'award: launchcode completion'
 
-print(name)
+first_name_cap = first_name.capitalize()
+last_name_cap = last_name.capitalize()
+award_location = note.find(":")
+print(award_location)
+after_award = note[award_location+2: len(note)]
+print(after_award)
